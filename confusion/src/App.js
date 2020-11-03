d0f80d74327071327dabaadea44f93be4ef8ed13
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-11-03 08:09:47 -0500

    creacion de menu de platos

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent'
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">
@@ -11,6 +12,7 @@ function App() {
           </NavbarBrand>
         </div>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
