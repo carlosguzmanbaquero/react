557a9d47bd03f9898943cbf48fdebbc1ea997f3c
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-11-05 06:57:58 -0500

    menu de plato y detalle de platos

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,20 +1,32 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent'
 import './App.css';
+import { DISHES } from './shared/dishes';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">
-            Confusion
-          </NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">
+              Confusion
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
