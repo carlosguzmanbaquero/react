261fa26032fd66280df9d38874d3cf4eab908475
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-11-18 20:09:59 -0500

    agregando al formulario redux form

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -9,6 +9,7 @@ import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
 const mapStateToProps = state => {
   return {
@@ -22,7 +23,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => ({
   
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => { dispatch(fetchDishes())}
+  fetchDishes: () => { dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 
 });
 
@@ -61,7 +63,7 @@ class Main extends Component{
             <Route path="/home" component={HomePage}></Route>
             <Route exact path="/menu" component={()=> <Menu dishes={this.props.dishes}/> }></Route>
             <Route exact path="/menu/:dishId" component={DishWithId}></Route>
-            <Route exact path='/contactus' component={Contact}/>
+            <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />}/>
             <Route exact path="/about" component={()=> <About leaders={this.props.leaders}/> }></Route>
             <Redirect to="/home"></Redirect>
           </Switch>
